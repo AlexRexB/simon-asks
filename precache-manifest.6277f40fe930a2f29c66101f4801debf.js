ad7ce89326656d5c05c0b7ba58de1ec9c0ee72ab
AlexRexB <alexrexb@gmail.com>
2020-03-04 23:30:34 -0500

    Updates

---
+++ precache-manifest.6277f40fe930a2f29c66101f4801debf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f95f2b08610a86081ad4db12613f2c",
+    "url": "/AlexRexB/simon-asks/index.html"
+  },
+  {
+    "revision": "a4879666fb9d7b4aaa31",
+    "url": "/AlexRexB/simon-asks/static/css/main.1cac0708.chunk.css"
+  },
+  {
+    "revision": "f8b002ded4b96b6d48fc",
+    "url": "/AlexRexB/simon-asks/static/js/2.af8e36bd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/AlexRexB/simon-asks/static/js/2.af8e36bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4879666fb9d7b4aaa31",
+    "url": "/AlexRexB/simon-asks/static/js/main.12ca49f1.chunk.js"
+  },
+  {
+    "revision": "11cfb8c05f1ed592b836",
+    "url": "/AlexRexB/simon-asks/static/js/runtime-main.b5609931.js"
+  }
+]);
